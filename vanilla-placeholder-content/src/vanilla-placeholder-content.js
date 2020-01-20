6154148127abd2eb0bfaf90fe496307a19fcaa37
Carlos Rojas <carlos.rojas@localhost>
2020-01-20 08:50:00 -0500

    WIP: adding properties

--- vanilla-placeholder-content/src/vanilla-placeholder-content.js
+++ vanilla-placeholder-content/src/vanilla-placeholder-content.js
@@ -1,2 +1,23 @@
-export class VanillaPlaceholderContent extends HTMLElement {}
+export class VanillaPlaceholderContent extends HTMLElement {
+    constructor() {
+        super();
+        const placeholder = document.createElement('placeholder');
+        placeholder.innerHTML = VanillaPlaceholderContent.template();
+        placeholder.style.height = this.getAttribute('height') || '50px';
+        placeholder.style.width = this.getAttribute('width') || '50px';
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(document.importNode(placeholder.content, true));
+    }
+
+    static template () {
+        return `
+        <style>
+        .placeholder {
+            background-color: red;
+        }
+        </style>
+        <div class='placeholder'>Placeholder</div>`;
+    }
+}
 customElements.define('vanilla-placeholder-content', VanillaPlaceholderContent);
