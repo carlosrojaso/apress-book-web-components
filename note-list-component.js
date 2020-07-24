d43eb4b5caa5fb30954f12019d98a81348725cf2
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2020-07-24 13:08:40 -0500

    adding note-list component

---
+++ note-list-component.js
@@ -0,0 +1,51 @@
+import {LitElement, html, css} from 'lit-element';
+
+/**
+ * A Note List Component
+ */
+export class NoteListComponent extends LitElement {
+
+  static get properties() {
+    return {
+      /**
+       * The attribute is an Array of elements.
+       */
+      notes: {
+        type: Array,
+        attribute: true,
+        reflect: true
+      }
+    };
+  }
+
+  constructor() {
+    super();
+    this.notes = this.getAttribute('notes') || [];
+    this.addEventListener('del-event', this.handleDelEvent);
+  }
+
+  attributeChangedCallback() {
+    this.notes = [...this.notes];
+    super.attributeChangedCallback();
+  }
+
+  render() {
+    return html`
+    ${this.notes.map(
+      (note, idx) => {
+        return html`
+        <note-list-item-component
+        note="${JSON.stringify(note)}"
+        idx="${idx}"></note-list-item-component>`;
+      }
+    )}
+    `;
+  }
+
+  handleDelEvent(e) {
+    console.log(e);
+    this.notes.splice(e.detail.idx, 1);
+  }
+}
+
+window.customElements.define('note-list-component', NoteListComponent);
