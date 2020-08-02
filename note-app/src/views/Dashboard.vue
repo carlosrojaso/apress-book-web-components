c74e9b0bf6e1c2b54e6f36e49e2a28144cbb2926
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2020-08-02 12:46:58 -0500

    Adding dummyData and delete

--- note-app/src/views/Dashboard.vue
+++ note-app/src/views/Dashboard.vue
@@ -1,26 +1,13 @@
 <!-- eslint-disable vue/no-deprecated-slot-attribute -->
 <template>
   <div>
-    <mwc-list multi>
-      <mwc-list-item twoline>
-        <span>Item 0</span>
-        <span slot="secondary">Secondary line</span>
+    <mwc-list v-for="(note) in notes" :key="note.id" multi>
+      <mwc-list-item twoline hasMeta>
+        <span>{{note.title}}</span>
+        <span slot="meta" class="material-icons" @click="handleDelete(note.id)">delete</span>
+        <span slot="secondary">{{note.description}}</span>
       </mwc-list-item>
       <li divider padded role="separator"></li>
-      <mwc-list-item twoline>
-        <span>Item 1</span>
-        <span slot="secondary">Secondary line</span>
-      </mwc-list-item>
-      <li divider padded role="separator"></li>
-      <mwc-list-item twoline>
-        <span>Item 2</span>
-        <span slot="secondary">Secondary line</span>
-      </mwc-list-item>
-      <li divider padded role="separator"></li>
-      <mwc-list-item twoline>
-        <span>Item 3</span>
-        <span slot="secondary">Secondary line</span>
-      </mwc-list-item>
     </mwc-list>
     <mwc-fab class="floatButton" mini icon="add"></mwc-fab>
   </div>
@@ -29,15 +16,28 @@
 import '@material/mwc-list/mwc-list';
 import '@material/mwc-list/mwc-list-item';
 import '@material/mwc-fab';
+import '@material/mwc-button';
+import { notesData } from '../utils/DummyData';
 
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  data() {
+    return {
+      notes: notesData
+    }
+  },
+  methods: {
+    handleDelete(id) {
+      const noteToDelete = this.notes.findIndex((item) => (item.id === id));
+      this.notes.splice(noteToDelete, 1);
+    }
+  },
 }
 </script>
 <style scoped>
-.floatButton {
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-}
+  .floatButton {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+  }
 </style>
